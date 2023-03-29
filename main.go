46656713e44dd4458c988dd56b2aa8723ee5d3df
ranggasuryap15 <ranggasurya.313@gmail.com>
2023-03-29 22:17:50 +0700

    dapat nilai 100 wkkw

--- main.go
+++ main.go
@@ -3,7 +3,24 @@ package main
 import "fmt"
 
 func GetPredicate(math, science, english, indonesia int) string {
-	return "" // TODO: replace this
+	var peringkat string
+	nilai := (math + science + english + indonesia) / 4
+	if nilai == 100 {
+		peringkat = "Sempurna"
+	} else if nilai >= 90 {
+		peringkat = "Sangat Baik"
+	} else if nilai >= 80 {
+		peringkat = "Baik"
+	} else if nilai >= 70 {
+		peringkat = "Cukup"
+	} else if nilai >= 60 {
+		peringkat = "Kurang"
+	} else if nilai < 60 {
+		peringkat = "Sangat kurang"
+	} else {
+		peringkat = "Nilai tidak boleh lebih dari 100 dan kurang dari 0"
+	}
+	return peringkat // TODO: replace this
 }
 
 // gunakan untuk melakukan debug
